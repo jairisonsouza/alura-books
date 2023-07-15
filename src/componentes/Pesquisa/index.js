b3a91143b8c6c8a28d55c04568b3935a0dad5893
jairisonsouza <jairo.tinformatica@gmail.com>
2023-07-15 20:33:54 -0300

    Configuração de inserção e remoção de favoritos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -2,6 +2,7 @@ import Input from "../Input"
 import styled from 'styled-components'
 import { useEffect, useState } from "react"
 import { getLivros } from "../../servicos/livros"
+import { postFavorito } from "../../servicos/favoritos"
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -55,6 +56,11 @@ function Pesquisa () {
         setLivros(LivrosDaAPI)
     }
 
+    async function insertFavorito(id) {
+        await postFavorito(id)
+        alert(`Livro de id:${id} inserido!`)
+    }
+
     return (
         <PesquisaContainer>
         <Titulo>Já sabe por onde começar?</Titulo>
@@ -68,7 +74,7 @@ function Pesquisa () {
             }}
         />
         { LivrosPesquisados.map( livro => (
-            <Resultado>
+            <Resultado onClick={() => insertFavorito(livro.id)}>
                 <img src={livro.src} alt="Imagem Livro"/>
                 <p>{livro.nome}</p>
             </Resultado>
